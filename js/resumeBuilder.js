52b1b5adf3f4bd591dc5563f97ff7dbe91be0cb8
Nupoor Nitin Kotasthane <nupoor292k@gmail.com>
2015-09-12 13:52:19 -0700

    Updating header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ //$("#main").append("Nupoor Nitin Kotasthane");
+ var formattedName = HTMLheaderName.replace("%data%", "Nupoor Nitin Kotasthane");
+ $("#header").append(formattedName);
+
+ var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer Intern");
+ $("#header").append(formattedRole);
